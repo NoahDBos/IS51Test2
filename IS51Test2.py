d0449aada78ef663dde5cdbd33be76fc86450f51
NoahDBos <70241751+NoahDBos@users.noreply.github.com>
2020-12-03 16:32:48 -0800

    structured english

---
+++ IS51Test2.py
@@ -0,0 +1,9 @@
+
+"""
+The program will take the students grades from the Final.txt and count how many 
+grades are in the file. Next it will return to the user how many grades there where.
+It will then add up all the grades and divide by the number of grades 
+there were to determine the class grade average and return that number to the user.
+Lastly, it will calculate the percentage of grades that are above the class average
+and return that number to the user.
+"""
